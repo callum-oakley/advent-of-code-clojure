d05da0454e93eb29b9b6ada685cfd783a51bae61
Callum Oakley <hello@callumoakley.net>
2021-10-15 21:13:20 +0100

    2015 23

---
+++ src/aoc/2015/23.clj
@@ -0,0 +1,28 @@
+(ns aoc.2015.23
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (mapv #(map read-string (re-seq #"\S+" %)) (str/split-lines s)))
+
+(defn run [m head registers]
+  (if-let [[op x y] (get m head)]
+    (case op
+      hlf (recur m (inc head) (update registers x quot 2))
+      tpl (recur m (inc head) (update registers x * 3))
+      inc (recur m (inc head) (update registers x inc))
+      jmp (recur m (+ x head) registers)
+      jie (recur m (if (even? (registers x)) (+ y head) (inc head)) registers)
+      jio (recur m (if (= 1 (registers x)) (+ y head) (inc head)) registers))
+    (registers 'b)))
+
+(defn part-1 []
+  (run (parse (slurp "input/2015/23")) 0 '{a 0 b 0}))
+
+(defn part-2 []
+  (run (parse (slurp "input/2015/23")) 0 '{a 1 b 0}))
+
+(deftest test-answers
+  (is (= 255 (part-1)))
+  (is (= 334 (part-2))))
