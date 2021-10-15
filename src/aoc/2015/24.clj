d7c76ef7a340dd30c7f8bb68eb9dfb4c11d80eaf
Callum Oakley <hello@callumoakley.net>
2021-10-16 00:40:47 +0100

    2015 24

---
+++ src/aoc/2015/24.clj
@@ -0,0 +1,28 @@
+(ns aoc.2015.24
+  (:require
+   [clojure.math.combinatorics :as comb]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn part-* [parts min-group-size packages]
+  (let [target (/ (apply + packages) parts)]
+    ;; Strictly speaking we should ensure the remaining packages can also be
+    ;; split. Turns out they can but checking is slow.
+    (if-let [groups (seq (filter #(= target (apply + %))
+                                 (comb/combinations packages min-group-size)))]
+      (apply min (map #(apply * %) groups))
+      (recur parts (inc min-group-size) packages))))
+
+(defn part-1 []
+  (->> "input/2015/24" slurp str/split-lines (map read-string) (part-* 3 1)))
+
+(defn part-2 []
+  (->> "input/2015/24" slurp str/split-lines (map read-string) (part-* 4 1)))
+
+(deftest test-part-*
+  (is (= 99 (part-* 3 1 [1 2 3 4 5 7 8 9 10 11])))
+  (is (= 44 (part-* 4 1 [1 2 3 4 5 7 8 9 10 11]))))
+
+(deftest test-answers
+  (is (= 11846773891 (part-1)))
+  (is (= 80393059 (part-2))))
