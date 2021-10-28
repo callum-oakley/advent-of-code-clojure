74eeb0ffd21e52dbfb58c18dc012f857ab68aa77
Callum Oakley <hello@callumoakley.net>
2021-10-28 22:41:01 +0100

    18

---
+++ src/aoc/2016/18.clj
@@ -0,0 +1,25 @@
+(ns aoc.2016.18
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn step [tiles]
+  (->> (concat [\.] tiles [\.])
+       (partition 3 1)
+       (map (fn [[left _ right]] (if (= left right) \. \^)))))
+
+(defn part-* [n tiles]
+  (->> (iterate step tiles) (take n) (apply concat) (filter #{\.}) count))
+
+(defn part-1 []
+  (part-* 40 (str/trim (slurp "input/2016/18"))))
+
+(defn part-2 []
+  (part-* 400000 (str/trim (slurp "input/2016/18"))))
+
+(deftest test-part-*
+  (is (= 38 (part-* 10 ".^^.^.^^^^"))))
+
+(deftest test-answers
+  (is (= 1987 (part-1)))
+  (is (= 19984714 (part-2))))
