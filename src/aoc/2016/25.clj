01f81361ffd2af91d42ae62d0e6e724104732958
Callum Oakley <hello@callumoakley.net>
2021-11-01 22:31:42 +0000

    2016 25

---
+++ src/aoc/2016/25.clj
@@ -0,0 +1,28 @@
+(ns aoc.2016.25
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (mapv #(mapv read-string (re-seq #"\S+" %)) (str/split-lines s)))
+
+(defn run [m head reg]
+  (when-let [[op x y] (get m head)]
+    (let [read #(if (int? %) % (reg %))]
+      (case op
+        cpy (recur m (inc head) (assoc reg y (read x)))
+        inc (recur m (inc head) (update reg x inc))
+        dec (recur m (inc head) (update reg x dec))
+        jnz (recur m (if (zero? (read x)) (inc head) (+ (read y) head)) reg)
+        out (cons (read x) (lazy-seq (run m (inc head) reg)))))))
+
+(defn part-1 []
+  (let [m (parse (slurp "input/2016/25"))
+        signal (apply concat (replicate 50 [0 1]))]
+    (some #(when (= signal
+                    (take (count signal) (run m 0 {'a % 'b 0 'c 0 'd 0})))
+             %)
+          (range))))
+
+(deftest test-answers
+  (is (= 198 (part-1))))
