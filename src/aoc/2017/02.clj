c51da0964cda5dc7026bee0ae8606a976068c5d9
Callum Oakley <hello@callumoakley.net>
2021-11-09 20:20:12 +0000

    2017 02

---
+++ src/aoc/2017/02.clj
@@ -0,0 +1,34 @@
+(ns aoc.2017.02
+  (:require
+   [clojure.math.combinatorics :as comb]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (->> s str/split-lines (map #(map read-string (re-seq #"\d+" %)))))
+
+(defn part-1* [sheet]
+  (apply + (map #(- (apply max %) (apply min %)) sheet)))
+
+(defn part-2* [sheet]
+  (->> sheet
+       (map #(some (fn [[a b]]
+                     (cond
+                       (int? (/ a b)) (/ a b)
+                       (int? (/ b a)) (/ b a)))
+                   (comb/combinations % 2)))
+       (apply +)))
+
+(defn part-1 []
+  (->> "input/2017/02" slurp parse part-1*))
+
+(defn part-2 []
+  (->> "input/2017/02" slurp parse part-2*))
+
+(deftest test-examples
+  (is (= 18 (part-1* [[5 1 9 5] [7 5 3] [2 4 6 8]])))
+  (is (= 9 (part-2* [[5 9 2 8] [9 4 7 3] [3 8 6 5]]))))
+
+(deftest test-answers
+  (is (= 44887 (part-1)))
+  (is (= 242 (part-2))))
