6297be69f5f5dfbb6e03155bca9f1d0841e47b13
Callum Oakley <hello@callumoakley.net>
2021-11-09 23:31:12 +0000

    2017 05

---
+++ src/aoc/2017/05.clj
@@ -0,0 +1,28 @@
+(ns aoc.2017.05
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn part-* [p jumps]
+  (loop [steps 0 head 0 jumps jumps]
+    (if (contains? jumps head)
+      (recur (inc steps)
+             (+ (jumps head) head)
+             (update jumps head (case p
+                                  1 inc
+                                  2 #(if (< % 3) (inc %) (dec %)))))
+      steps)))
+
+(defn part-1 []
+  (->> "input/2017/05" slurp str/split-lines (mapv read-string) (part-* 1)))
+
+(defn part-2 []
+  (->> "input/2017/05" slurp str/split-lines (mapv read-string) (part-* 2)))
+
+(deftest test-examples
+  (is (= 5 (part-* 1 [0 3 0 1 -3])))
+  (is (= 10 (part-* 2 [0 3 0 1 -3]))))
+
+(deftest test-answers
+  (is (= 342669 (part-1)))
+  (is (= 25136209 (part-2))))
