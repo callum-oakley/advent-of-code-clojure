8800ae133bda0184164b54fae1b37788b60e39aa
Callum Oakley <hello@callumoakley.net>
2021-11-11 22:56:51 +0000

    2017 06

---
+++ src/aoc/2017/06.clj
@@ -0,0 +1,38 @@
+(ns aoc.2017.06
+  (:require
+   [clojure.test :refer [deftest is]]))
+
+(defn target-bank [banks]
+  (let [max-bank (apply max banks)]
+    (first (keep-indexed (fn [i blocks] (when (= max-bank blocks) i)) banks))))
+
+(defn redistribute [banks]
+  (let [t (target-bank banks)]
+    (loop [blocks (banks t)
+           banks (assoc banks t 0)
+           i (mod (inc t) (count banks))]
+      (if (pos? blocks)
+        (recur (dec blocks) (update banks i inc) (mod (inc i) (count banks)))
+        banks))))
+
+(defn part-* [step seen banks]
+  (if-let [seen-at-step (seen banks)]
+    [step (- step seen-at-step)]
+    (recur (inc step) (assoc seen banks step) (redistribute banks))))
+
+(defn part-1 []
+  (->> "input/2017/06" slurp (re-seq #"\d+") (mapv read-string)
+       (part-* 0 {}) first))
+
+(defn part-2 []
+  (->> "input/2017/06" slurp (re-seq #"\d+") (mapv read-string)
+       (part-* 0 {}) second))
+
+(deftest test-examples
+  (is (= [[0 2 7 0] [2 4 1 2] [3 1 2 3] [0 2 3 4] [1 3 4 1] [2 4 1 2]]
+         (take 6 (iterate redistribute [0 2 7 0]))))
+  (is (= [5 4] (part-* 0 {} [0 2 7 0]))))
+
+(deftest test-answers
+  (is (= 11137 (part-1)))
+  (is (= 1037 (part-2))))
