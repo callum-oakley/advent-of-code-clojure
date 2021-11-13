0521341f2cb549b9f71194cdb01260eb9e6de45e
Callum Oakley <hello@callumoakley.net>
2021-11-13 19:11:29 +0000

    2017 08

---
+++ src/aoc/2017/08.clj
@@ -0,0 +1,32 @@
+(ns aoc.2017.08
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (->> s str/split-lines (map #(map read-string (re-seq #"\S+" %)))))
+
+(defn run [instructions]
+  (reductions (fn [reg [a op x _ b cmp y]]
+                (if ((case cmp == = != not= (resolve cmp)) (get reg b 0) y)
+                  (update reg a (fnil (case op inc + dec -) 0) x)
+                  reg))
+              {}
+              instructions))
+
+(defn part-1 []
+  (->> "input/2017/08" slurp parse run last vals (apply max)))
+
+(defn part-2 []
+  (->> "input/2017/08" slurp parse run (mapcat vals) (apply max)))
+
+(deftest test-example
+  (let [instructions (parse "b inc 5 if a > 1
+                             a inc 1 if b < 5
+                             c dec -10 if a >= 1
+                             c inc -20 if c == 10")]
+    (is (= [{} {} '{a 1} '{a 1 c 10} '{a 1 c -10}] (run instructions)))))
+
+(deftest test-answers
+  (is (= 5946 (part-1)))
+  (is (= 6026 (part-2))))
