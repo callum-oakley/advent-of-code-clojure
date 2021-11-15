ac6c2abbbd1a3448f2df2439bccb92c6583687e0
Callum Oakley <hello@callumoakley.net>
2021-11-15 18:17:20 +0000

    2017 13

---
+++ src/aoc/2017/13.clj
@@ -0,0 +1,31 @@
+(ns aoc.2017.13
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (->> s str/split-lines (map #(map read-string (re-seq #"\d+" %)))))
+
+(defn caught [delay scanners]
+  (filter (fn [[depth range]]
+            (zero? (mod (+ delay depth) (* 2 (dec range)))))
+          scanners))
+
+(defn part-1 []
+  (->> "input/2017/13" slurp parse (caught 0) (map #(apply * %)) (apply +)))
+
+(defn part-2* [scanners]
+  ;; Testing scanners in order of increasing range saves us some time
+  (let [scanners (sort-by second scanners)]
+    (first (remove #(seq (caught % scanners)) (range)))))
+
+(defn part-2 []
+  (->> "input/2017/13" slurp parse part-2*))
+
+(deftest test-example
+  (is (= [[0 3] [6 4]] (caught 0 [[0 3] [1 2] [4 4] [6 4]])))
+  (is (= 10 (part-2* [[0 3] [1 2] [4 4] [6 4]]))))
+
+(deftest test-answers
+  (is (= 3184 (part-1)))
+  (is (= 3878062 (part-2))))
