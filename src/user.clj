1f0248ce336a078dc926ccca06f6433c50d62fa5
Callum Oakley <hello@callumoakley.net>
2021-11-25 12:41:01 +0000

    check answers on run

--- src/user.clj
+++ src/user.clj
@@ -13,11 +13,49 @@
 (defn download-input [year day]
   (let [path (format "input/%d/%02d" year day)]
     (when-not (.exists (io/file path))
+      (io/make-parents path)
       (spit path
             (:body
              (client/get
               (format "https://adventofcode.com/%d/day/%d/input" year day)
-              {:headers {"cookie" (str "session=" (slurp ".session"))}}))))))
+              {:headers {:cookie (str "session=" (slurp ".session"))}}))))))
+
+(defn get-answer* [year day part]
+  (nth (map
+        second
+        (re-seq #"Your puzzle answer was <code>([^<]+)</code>"
+                (:body
+                 (client/get
+                  (format "https://adventofcode.com/%d/day/%d" year day)
+                  {:headers {:cookie (str "session=" (slurp ".session"))}}))))
+       (dec part)
+       nil))
+
+(defn get-answer [year day part]
+  (let [path (format "answers/%d/%02d/%d" year day part)]
+    (if (.exists (io/file path))
+      (slurp path)
+      (when-let [answer (get-answer* year day part)]
+        (io/make-parents path)
+        (spit path answer)
+        answer))))
+
+(defn submit-answer [year day part answer]
+  (:body
+   (client/post
+    (format "https://adventofcode.com/%d/day/%d/answer" year day)
+    {:headers {:cookie (str "session=" (slurp ".session"))}
+     :form-params {:level part :answer answer}})))
+
+(defn check-answer [year day part answer]
+  (if-let [target (get-answer year day part)]
+    (if (= target (str answer)) "OK" "WRONG")
+    (let [res (submit-answer year day part answer)]
+      (cond
+        (re-find #"That's the right answer" res) "OK"
+        (re-find #"That's not the right answer" res) "WRONG"
+        (re-find #"You gave an answer too recently" res) "WAIT"
+        :else (throw (ex-info "Unexpected response" {:res res}))))))
 
 (defn run-tests
   ([]
@@ -27,8 +65,12 @@
      (run-tests default-year year)
      (test/run-all-tests (re-pattern (format "aoc\\.%d\\..*" year)))))
   ([year day]
-   (download-input year day)
    (test/run-all-tests (re-pattern (format "aoc\\.%d\\.%02d" year day)))))
+
+(defn run-scrap [year day]
+  (when-let [f (resolve (symbol (format "aoc.%d.%02d/scrap" year day)))]
+    (download-input year day)
+    (println (f))))
 
 (defn run
   ([]
@@ -41,6 +83,7 @@
    (if (<= year 25)
      (run default-year year day)
      (do
+       (run-scrap year day)
        (run year day 1)
        (run year day 2))))
   ([year day part]
@@ -48,9 +91,10 @@
      (when-let [f (resolve sym)]
        (download-input year day)
        (let [start (System/currentTimeMillis)
-             res (f)
-             duration (double (/ (- (System/currentTimeMillis) start) 1000))]
-         (println (format "%s %7.3fs   %s" sym duration res)))))))
+             answer (f)
+             duration (double (/ (- (System/currentTimeMillis) start) 1000))
+             check (check-answer year day part answer)]
+         (println (format "%s %7.3fs   %-44s %s" sym duration answer check)))))))
 
 (defn rrr [& args]
   (repl/refresh)
