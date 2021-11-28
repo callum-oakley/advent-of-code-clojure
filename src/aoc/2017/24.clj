49f1e63628e3bb8b40313345346b6037255d7f15
Callum Oakley <hello@callumoakley.net>
2021-11-28 13:17:03 +0000

    2017 24

---
+++ src/aoc/2017/24.clj
@@ -0,0 +1,45 @@
+(ns aoc.2017.24
+  (:require
+   [aoc.search :as search]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (reduce (fn [m component]
+            (let [[a b] (->> component (re-seq #"\d+") (map read-string))]
+              (-> m
+                  (update a (fnil conj #{}) b)
+                  (update b (fnil conj #{}) a))))
+          {}
+          (str/split-lines s)))
+
+(defn bridges [components]
+  (map :bridge
+       (search/dft {:components components
+                    :bridge [0]}
+                   (fn [{:keys [components bridge] :as state}]
+                     (let [from (peek bridge)]
+                       (map (fn [to]
+                              {:components (-> components
+                                               (update from disj to)
+                                               (update to disj from))
+                               :bridge (conj bridge from to)})
+                            (components from)))))))
+
+(defn part-1* [components]
+  (->> components bridges (map #(apply + %)) (apply max)))
+
+(defn part-1 []
+  (-> "input/2017/24" slurp parse part-1*))
+
+(defn part-2* [components]
+  (let [bs (bridges components)
+        longest (->> bs (map count) (apply max))]
+    (->> bs (filter #(= longest (count %))) (map #(apply + %)) (apply max))))
+
+(defn part-2 []
+  (-> "input/2017/24" slurp parse part-2*))
+
+(deftest test-examples
+  (is (= 31 (part-1* (parse "0/2\n2/2\n2/3\n3/4\n3/5\n0/1\n10/1\n9/10"))))
+  (is (= 19 (part-2* (parse "0/2\n2/2\n2/3\n3/4\n3/5\n0/1\n10/1\n9/10")))))
