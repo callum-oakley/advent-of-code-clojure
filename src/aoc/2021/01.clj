152697532000a3f54b561c3f7a8b05dbd9bdee4d
Callum Oakley <hello@callumoakley.net>
2021-12-01 06:30:00 +0000

    2021 01

---
+++ src/aoc/2021/01.clj
@@ -0,0 +1,20 @@
+(ns aoc.2021.01
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn part-1* [xs]
+  (->> xs (partition 2 1) (filter (fn [[a b]] (< a b))) count))
+
+(defn part-2* [xs]
+  (->> xs (partition 3 1) (map #(apply + %)) part-1*))
+
+(defn part-1 []
+  (->> "input/2021/01" slurp str/split-lines (map read-string) part-1*))
+
+(defn part-2 []
+  (->> "input/2021/01" slurp str/split-lines (map read-string) part-2*))
+
+(deftest test-example
+  (is (= 7 (part-1* [199 200 208 210 200 207 240 269 260 263])))
+  (is (= 5 (part-2* [199 200 208 210 200 207 240 269 260 263]))))
