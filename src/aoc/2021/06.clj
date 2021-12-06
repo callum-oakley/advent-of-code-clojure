19792c945305c3e79c2eada183f0a9aa6ffb47e9
Callum Oakley <hello@callumoakley.net>
2021-12-06 09:59:35 +0000

    2021 6, actually rotate the vector. it's clearer and just as fast

--- src/aoc/2021/06.clj
+++ src/aoc/2021/06.clj
@@ -6,18 +6,11 @@
   (let [f (->> s (re-seq #"\d+") (map read-string) frequencies)]
     (mapv #(get f % 0) (range 9))))
 
-;; Represent fish populations in a circular buffer of length 9, with a pointer
-;; to the fish with internal timer 0. Then incrementing the pointer is
-;; equivalent to ageing every fish by 1 day while simultaneously producing our
-;; "new" timer-8 generation. The only update to the buffer itself we need to
-;; make on each day is to move the timer-0 generation 7 days forwards.
 (defn simulate [fish]
-  (iterate (fn [[fish i]]
-             [(update fish (mod (+ i 7) 9) + (fish i)) (mod (inc i) 9)])
-           [fish 0]))
+  (iterate #(conj (update (subvec % 1) 6 + (% 0)) (% 0)) fish))
 
 (defn part-* [days fish]
-  (apply + (first (nth (simulate fish) days))))
+  (apply + (nth (simulate fish) days)))
 
 (defn part-1 []
   (->> "input/2021/06" slurp parse (part-* 80)))
