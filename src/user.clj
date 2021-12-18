e12fe8ca7cfc06e8b957ff20ab033e04d8687fa4
Callum Oakley <hello@callumoakley.net>
2021-12-18 15:38:51 +0000

    Revert "remove scrap"
    This reverts commit 9014bef6c0bf455ecb48406227347dc4faa044e0.

--- src/user.clj
+++ src/user.clj
@@ -68,6 +68,11 @@
   ([year day]
    (test/run-all-tests (re-pattern (format "aoc\\.%d\\.%02d" year day)))))
 
+(defn run-scrap [year day]
+  (when-let [f (resolve (symbol (format "aoc.%d.%02d/scrap" year day)))]
+    (download-input year day)
+    (pp/pprint (f))))
+
 (defn run
   ([]
    (let [start (System/currentTimeMillis)
@@ -85,6 +90,7 @@
    (if (<= year 25)
      (run default-year year day)
      (do
+       (run-scrap year day)
        (run year day 1)
        (run year day 2))))
   ([year day part]
