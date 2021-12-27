6d2d0543e4454edbee2166fd02acec9913f44e1e
Callum Oakley <hello@callumoakley.net>
2021-12-27 12:44:21 +0000

    2021 23 a*

--- src/aoc/2021/23.clj
+++ src/aoc/2021/23.clj
@@ -3,6 +3,7 @@
    [aoc.grid :as grid]
    [aoc.search :as search]
    [aoc.vector :refer [manhattan-distance]]
+   [clojure.java.math :as math]
    [clojure.string :as str]
    [clojure.test :refer [deftest is]]))
 
@@ -17,30 +18,32 @@
 (def hall
   #{[1 1] [1 2] [1 4] [1 6] [1 8] [1 10] [1 11]})
 
-(defn room [grid amphipod]
-  (->> (keys grid)
-       (filter (fn [[y x]]
-                 (and (< 1 y) (= (case amphipod \A 3 \B 5 \C 7 \D 9) x))))
-       (sort #(compare %2 %1))))
+(def room-x
+  {\A 3 \B 5 \C 7 \D 9})
+
+(defn room [room-height amphipod]
+  (map (fn [y] [y (room-x amphipod)]) (range (inc room-height) 1 -1)))
 
 (def step-energy
   {\A 1 \B 10 \C 100 \D 1000})
 
-(defn path [from to]
-  (let [[[hall-y hall-x] [room-y room-x]] (sort [from to])
-        [min-x max-x] (sort [hall-x room-x])]
-    (concat (map (fn [x] [hall-y x]) (range min-x (inc max-x)))
-            (map (fn [y] [y room-x]) (range 2 (inc room-y))))))
+(def path
+  (memoize
+   (fn [from to]
+     (let [[[hall-y hall-x] [room-y room-x]] (sort [from to])
+           [min-x max-x] (sort [hall-x room-x])]
+       (concat (map (fn [x] [hall-y x]) (range min-x (inc max-x)))
+               (map (fn [y] [y room-x]) (range 2 (inc room-y))))))))
 
 (defn clear? [grid path]
   (= 1 (count (keep grid path))))
 
-(defn vacancy [grid amphipod]
-  (when (->> amphipod (room grid) (keep grid) (every? #{amphipod}))
-    (->> amphipod (room grid) (remove grid) first)))
+(defn vacancy [room-height grid amphipod]
+  (when (->> amphipod (room room-height) (keep grid) (every? #{amphipod}))
+    (->> amphipod (room room-height) (remove grid) first)))
 
-(defn all-home? [grid amphipod]
-  (every? #(= amphipod (grid %)) (room grid amphipod)))
+(defn all-home? [room-height grid amphipod]
+  (every? #(= amphipod (grid %)) (room room-height amphipod)))
 
 (defn move [{grid :grid :as state} amphipod from to]
   (when (clear? grid (path from to))
@@ -49,33 +52,48 @@
         (update :energy + (* (step-energy amphipod)
                              (manhattan-distance from to))))))
 
-(defn moves [{grid :grid :as state}]
+(defn moves [room-height {grid :grid :as state}]
   (apply concat
          (keep (fn [from]
                  (when-let [amphipod (grid from)]
-                   (when-let [to (vacancy grid amphipod)]
+                   (when-let [to (vacancy room-height grid amphipod)]
                      (move state amphipod from to))))
                hall)
          (keep (fn [from]
                  (when-let [amphipod (grid from)]
                    (keep (fn [to] (move state amphipod from to)) hall)))
-               (mapcat #(room grid %) (remove #(all-home? grid %) "ABCD")))))
+               (mapcat #(room room-height %)
+                       (remove #(all-home? room-height grid %) "ABCD")))))
 
-(defn part-* [grid]
-  (:energy (search/dijkstra :energy
-                            {:grid grid :energy 0}
-                            moves
-                            (fn [{:keys [grid]}]
-                              (every? #(all-home? grid %) "ABCD"))
-                            :grid)))
+(defn heuristic [room-height grid]
+  (first
+   (reduce-kv (fn [[energy depth] [y x] amphipod]
+                (if (and amphipod (not= (room-x amphipod) x))
+                  [(+ energy (* (step-energy amphipod)
+                                (+ (math/abs (- (room-x amphipod) x))
+                                   y
+                                   (depth amphipod))))
+                   (update depth amphipod inc)]
+                  [energy depth]))
+              [0 {\A 0 \B 0 \C 0 \D 0}]
+              grid)))
+
+(defn part-* [room-height grid]
+  (:energy (search/a* :energy
+                      #(heuristic room-height (:grid %))
+                      {:grid grid :energy 0}
+                      #(moves room-height %)
+                      (fn [{:keys [grid]}]
+                        (every? #(all-home? room-height grid %) "ABCD"))
+                      :grid)))
 
 (defn part-1 []
-  (->> "input/2021/23" slurp parse part-*))
+  (->> "input/2021/23" slurp parse (part-* 2)))
 
 (defn part-2 []
-  (->> "input/2021/23" slurp unfold parse part-*))
+  (->> "input/2021/23" slurp unfold parse (part-* 4)))
 
 (deftest test-example
   (let [sample "#############\n#...........#\n###B#C#B#D###\n  #A#D#C#A#"]
-    (is (= 12521 (part-* (parse sample))))
-    (is (= 44169 (part-* (parse (unfold sample)))))))
+    (is (= 12521 (part-* 2 (parse sample))))
+    (is (= 44169 (part-* 4 (parse (unfold sample)))))))
