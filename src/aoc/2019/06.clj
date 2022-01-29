c496dab5fe8e1770c26a0f33fa14954e49a1c117
Callum Oakley <hello@callumoakley.net>
2022-01-29 18:28:53 +0000

    2019 6

---
+++ src/aoc/2019/06.clj
@@ -0,0 +1,32 @@
+(ns aoc.2019.06
+  (:require
+   [clojure.set :as set]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (->> s (re-seq #"\w+") (partition 2) (map (comp vec reverse)) (into {})))
+
+(defn orbits* [orbits obj]
+  (if-let [o (orbits obj)]
+    (conj (orbits* orbits o) o)
+    #{}))
+
+(defn part-1* [orbits]
+  (->> orbits keys (map #(count (orbits* orbits %))) (apply +)))
+
+(defn symmetric-diff [a b]
+  (set/difference (set/union a b) (set/intersection a b)))
+
+(defn part-2* [orbits]
+  (count (symmetric-diff (orbits* orbits "YOU") (orbits* orbits "SAN"))))
+
+(defn part-1 []
+  (->> "input/2019/06" slurp parse part-1*))
+
+(defn part-2 []
+  (->> "input/2019/06" slurp parse part-2*))
+
+(deftest test-example
+  (is (= 42 (part-1* (parse "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L"))))
+  (is (= 4 (part-2* (parse "COM)B B)C C)D D)E E)F B)G G)H
+                            D)I E)J J)K K)L K)YOU I)SAN")))))
