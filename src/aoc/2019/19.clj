889c770a6ae47033e59c12e1a2992fae3f2b4204
Callum Oakley <hello@callumoakley.net>
2022-04-17 11:07:38 +0100

    2019 19

---
+++ src/aoc/2019/19.clj
@@ -0,0 +1,20 @@
+(ns aoc.2019.19
+  (:require
+   [aoc.2019.intcode :as i]
+   [clojure.test :refer [deftest is]]))
+
+(defn part-1 []
+  (let [mem (i/load "input/2019/19")
+        beam? #(= [1] (i/run-io mem %))]
+    (->> (for [x (range 50) y (range 50)] [x y])
+         (filter beam?)
+         count)))
+
+(defn part-2 []
+  (let [mem (i/load "input/2019/19")
+        beam? #(= [1] (i/run-io mem %))]
+    (loop [x 0 y 0]
+      (cond
+        (not (beam? [(+ 99 x) y])) (recur x (inc y))
+        (not (beam? [x (+ 99 y)])) (recur (inc x) y)
+        :else (+ (* 10000 x) y)))))
