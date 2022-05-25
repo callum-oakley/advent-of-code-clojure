fce0746f8b9b3aa1a7e153a6115d83f12cbfac77
Callum Oakley <hello@callumoakley.net>
2022-05-25 20:52:22 +0100

    2019 13 interactive

--- src/aoc/2019/13.clj
+++ src/aoc/2019/13.clj
@@ -2,15 +2,17 @@
   (:require
    [aoc.2019.intcode :as i]))
 
-(defn play [& {:keys [slow? println]}]
+(defn play [& {:keys [interactive?]}]
   (loop [[out vm] (i/io (i/run (assoc (i/load "input/2019/13") 0 2)) [] 3)
          pad nil ball nil score nil]
     (if-let [[x y t] (seq out)]
       (do
-        (println (if (= [-1 0] [x y])
-                   (format "\u009B%s;%sH%s\u0007" 1 2 t)
-                   (format "\u009B%s;%sH%s" (+ 2 y) (+ 2 x)
-                           (case t 0 \space 1 \# 2 \= 3 \- 4 \o))))
+        (when interactive?
+          (println (if (= [-1 0] [x y])
+                     (format "\u009B%s;%sH%s\u0007" 1 2 t)
+                     (format "\u009B%s;%sH%s" (+ 2 y) (+ 2 x)
+                             (case t 0 \space 1 \# 2 \= 3 \- 4 \o))))
+          (Thread/sleep 5))
         (cond
           (= [-1 0] [x y]) (recur (i/io vm [] 3) pad ball t)
           (= 3 t) (recur (i/io vm [] 3) x ball score)
@@ -26,9 +28,9 @@
                  (partition 3 (i/run-io (i/load "input/2019/13") [])))))
 
 (defn part-2 []
-  (play :slow? false :println (constantly nil)))
+  (play :interactive? false))
 
 (defn -main []
   (println "\u009B?25l\u009B2J")
-  (play :slow? true :println println)
+  (play :interactive? true)
   (println (format "\u009B?25h\u009B%d;%dH" 28 0)))
