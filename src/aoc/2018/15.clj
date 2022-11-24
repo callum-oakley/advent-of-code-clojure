6b89fbe9052007dd24ff4d296c092b80978fd7c5
Callum Oakley <hello@callumoakley.net>
2022-11-25 00:03:13 +0900

    2018 15

--- src/aoc/2018/15.clj
+++ src/aoc/2018/15.clj
@@ -14,60 +14,87 @@
 (defn in-range? [cave pos target-type]
   (some #(= target-type (:type (cave %))) (g/adjacent pos)))
 
-(defn path [cave pos target-type]
-  (:path (search/dijkstra (juxt :dist :pos)
-                          {:dist 0 :pos pos :path []}
-                          (fn [state]
-                            (keep #(when (= :empty (:type (cave %)))
-                                     (-> state
-                                         (update :dist inc)
-                                         (assoc :pos %)
-                                         (update :path conj %)))
-                                  (sort (g/adjacent (:pos state)))))
-                          #(in-range? cave (:pos %) target-type)
-                          :pos)))
+(defn first-step [cave pos target-type]
+  (:first-step
+   ;; The cost function is fiddly, here's everything it needs to cover:
+   ;; - To move, the unit first considers the squares that are in range and
+   ;;   determines which of those squares it could reach in the fewest steps.
+   ;; - If multiple squares are in range and tied for being reachable in the
+   ;;   fewest steps, the square which is first in reading order is chosen.
+   ;; - If multiple steps would put the unit equally closer to its destination,
+   ;;   the unit chooses the step which is first in reading order.
+   ;; Missing either of the last two steps produces correct results for all the
+   ;; examples, but fails on the problem proper...
+   (search/dijkstra (juxt :dist :pos :first-step)
+                    {:dist 0 :pos pos}
+                    (fn [state]
+                      (keep (fn [step]
+                              (when (= :empty (:type (cave step)))
+                                (-> state
+                                    (update :dist inc)
+                                    (assoc :pos step)
+                                    (update :first-step #(or % step)))))
+                            (sort (g/adjacent (:pos state)))))
+                    #(in-range? cave (:pos %) target-type)
+                    :pos)))
 
 (defn move [cave pos target-type]
-  (if-let [pos* (first (path cave pos target-type))]
+  (if-let [pos* (first-step cave pos target-type)]
     [(assoc cave pos {:type :empty} pos* (cave pos)) pos*]
     [cave pos]))
 
 (defn attack [cave pos target-type]
   (if-let [target (some->> (g/adjacent pos)
                            (filter #(= target-type (:type (cave %))))
-                           ;; min-key is last wins so reverse
-                           seq sort reverse
-                           (apply min-key #(:hp (cave %))))]
+                           (sort-by (juxt #(:hp (cave %)) identity))
+                           first)]
     (let [hp (- (:hp (cave target)) (:ap (cave pos)))]
       (if (pos? hp)
         (assoc-in cave [target :hp] hp)
-        (assoc cave target {:type :empty})))
+        (if (< 3 (:ap (cave target)))
+          (reduced (reduced :unacceptable))
+          (assoc cave target {:type :empty}))))
     cave))
 
-(defn turn [cave pos target-type]
+(defn turn [r cave pos target-type]
   (if (some #{target-type} (map :type (vals cave)))
-    (let [[cave pos] (move cave pos target-type)]
-      (assoc-in (attack cave pos target-type) [pos :gone?] true))
-    (reduced (reduced cave))))
+    (let [[cave pos] (move cave pos target-type)
+          cave (attack cave pos target-type)]
+      (if (reduced? cave)
+        cave
+        (assoc-in cave [pos :gone?] true)))
+    (reduced (reduced (* r (apply + (keep :hp (vals cave))))))))
 
-(defn round [cave]
+(defn round [r cave]
   (reduce
    (fn [cave pos]
      (cond
        (or (= :empty (:type (cave pos))) (:gone? (cave pos))) cave
-       (= :goblin (:type (cave pos))) (turn cave pos :elf)
-       (= :elf (:type (cave pos))) (turn cave pos :goblin)))
+       (= :goblin (:type (cave pos))) (turn r cave pos :elf)
+       (= :elf (:type (cave pos))) (turn r cave pos :goblin)))
    cave
    (sort (keys cave))))
 
 (defn part-1* [cave]
-  (loop [cave (round cave) rounds 0]
+  (loop [cave cave r 0]
     (if (reduced? cave)
-      (* rounds (apply + (keep :hp (vals @cave))))
-      (recur (round (update-vals cave #(dissoc % :gone?))) (inc rounds)))))
+      @cave
+      (recur (round r (update-vals cave #(dissoc % :gone?))) (inc r)))))
+
+(defn part-2* [cave]
+  (->> (iterate inc 4)
+       (map (fn [ap]
+              (part-1* (update-vals
+                        cave
+                        #(if (= :elf (:type %)) (assoc % :ap ap) %)))))
+       (remove #{:unacceptable})
+       first))
 
 (defn part-1 []
-  (-> "input/2018/15" slurp parse part-1*))
+  (->> "input/2018/15" slurp parse part-1*))
+
+(defn part-2 []
+  (->> "input/2018/15" slurp parse part-2*))
 
 (deftest test-examples
   (is (= 27730 (part-1* (parse ".G...\n...EG\n.#.#G\n..G#E\n....."))))
@@ -75,4 +102,9 @@
   (is (= 39514 (part-1* (parse "E..EG\n.#G.E\nE.##E\nG..#.\n..E#."))))
   (is (= 27755 (part-1* (parse "E.G#.\n.#G..\nG.#.G\nG..#.\n...E."))))
   (is (= 28944 (part-1* (parse ".E...\n.#..G\n.###.\nE#G#G\n...#G"))))
-  (is (= 18740 (part-1* (parse "G......\n.E.#...\n..##..G\n...##..\n...#...\n.G...G.\n.....G.")))))
+  (is (= 18740 (part-1* (parse "G......\n.E.#...\n..##..G\n...##..\n...#...\n.G...G.\n.....G."))))
+  (is (= 4988 (part-2* (parse ".G...\n...EG\n.#.#G\n..G#E\n....."))))
+  (is (= 31284 (part-2* (parse "E..EG\n.#G.E\nE.##E\nG..#.\n..E#."))))
+  (is (= 3478 (part-2* (parse "E.G#.\n.#G..\nG.#.G\nG..#.\n...E."))))
+  (is (= 6474 (part-2* (parse ".E...\n.#..G\n.###.\nE#G#G\n...#G"))))
+  (is (= 1140 (part-2* (parse "G......\n.E.#...\n..##..G\n...##..\n...#...\n.G...G.\n.....G.")))))
