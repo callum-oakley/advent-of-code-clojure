7a407ef65aa3678eba6831ca5739df9769560bba
Callum Oakley <hello@callumoakley.net>
2022-12-06 07:10:12 +0200

    2022 06

---
+++ src/aoc/2022/06.clj
@@ -0,0 +1,27 @@
+(ns aoc.2022.06
+  (:require
+   [clojure.test :refer [deftest is]]))
+
+(defn marker [m signal]
+  (loop [signal signal i 0]
+    (if (apply distinct? (take m signal))
+      (+ i m)
+      (recur (rest signal) (inc i)))))
+
+(defn part-1 []
+  (->> "input/2022/06" slurp (marker 4)))
+
+(defn part-2 []
+  (->> "input/2022/06" slurp (marker 14)))
+
+(deftest test-example
+  (is (= 7 (marker 4 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
+  (is (= 5 (marker 4 "bvwbjplbgvbhsrlpgdmjqwftvncz")))
+  (is (= 6 (marker 4 "nppdvjthqldpwncqszvftbrmjlhg")))
+  (is (= 10 (marker 4 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")))
+  (is (= 11 (marker 4 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")))
+  (is (= 19 (marker 14 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")))
+  (is (= 23 (marker 14 "bvwbjplbgvbhsrlpgdmjqwftvncz")))
+  (is (= 23 (marker 14 "nppdvjthqldpwncqszvftbrmjlhg")))
+  (is (= 29 (marker 14 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")))
+  (is (= 26 (marker 14 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))))
