0dd70c68a92485216f7bd41ae4068a8709d2a10a
Callum Oakley <hello@callumoakley.net>
2022-12-06 09:16:20 +0900

    2018 21 (faster)

--- src/aoc/2018/21.clj
+++ src/aoc/2018/21.clj
@@ -1,9 +1,13 @@
 (ns aoc.2018.21
   (:require
    [aoc.2018.16 :refer [tick]]
-   [aoc.2018.19 :refer [parse]]))
+   [aoc.2018.19 :refer [parse]]
+   [clojure.test :refer [deftest is]]))
 
-;; TODO port to clojure instead of emulating
+;; The program halts after the test on line 28 if r0 is equal to r5. r0 doesn't
+;; otherwise feature in the calculation, so by running the program and noting r5
+;; each time we reach line 28, we produce a sequence of valid settings for r0
+;; to cause the program to halt.
 (defn valid []
   (let [[ip instructions] (parse (slurp "input/2018/21"))]
     ((fn go [reg]
@@ -14,8 +18,30 @@
             (go reg*)))))
      [0 0 0 0 0 0])))
 
+;; Part 2 requires us to find the last element of the above sequence before a
+;; repeat, and this turns out to be fairly slow. Reimplementing the process in
+;; Clojure gives a much needed speed boost. See corresponding line numbers in
+;; comments (lines 01-07 are the bitwise AND test).
+(defn valid* []
+  ((fn go [r2 r5]
+     (lazy-seq
+      (let [r4 (bit-and r2 255)                    ; 08
+            r5 (+ r5 r4)                           ; 09
+            r5 (bit-and r5 16777215)               ; 10
+            r5 (* r5 65899)                        ; 11
+            r5 (bit-and r5 16777215)]              ; 12
+        (if (< r2 256)                             ; 13-17
+          (cons r5 (go (bit-or r5 65536) 7571367)) ; 28-30, 06, 07
+          (loop [r4 0]                             ;
+            (let [r3 (+ r4 1)                      ; 18
+                  r3 (* r3 256)]                   ; 19
+              (if (< r2 r3)                        ; 20-23
+                (go r4 r5)                         ; 26-27
+                (recur (inc r4)))))))))            ; 24-25
+   65536 7571367))
+
 (defn part-1 []
-  (first (valid)))
+  (first (valid*)))
 
 (defn part-2 []
   (reduce (fn [[seen values] value]
@@ -23,37 +49,40 @@
               (reduced (peek values))
               [(conj seen value) (conj values value)]))
           [#{} []]
-          (valid)))
+          (valid*)))
+
+(deftest test-valid*
+  (is (= (take 10 (valid)) (take 10 (valid*)))))
 
 ; #ip 1
-; 00: seti 123 0 5      |
-; 01: bani 5 456 5      |
-; 02: eqri 5 72 5       |
-; 03: addr 5 1 1        |
-; 04: seti 0 0 1        |
-; 05: seti 0 9 5        \ when test fails, jump to 2 and loop forever
-; 06: bori 5 65536 2    \ r2 = r5 ^ 65536 (72 ^ 65536 = 65608)
-; 07: seti 7571367 9 5  \ r5 = 7571367
-; 08: bani 2 255 4      \ r4 = r2 & 255 (65608 & 255 = 72)
-; 09: addr 5 4 5        \ r5 = r5 + r4 (7571367 + 72 = 7571439)
-; 10: bani 5 16777215 5 \ r5 = r5 & 16777215 (7571439 & 16777215 = 7571439)
-; 11: muli 5 65899 5    \ r5 = r5 * 65899 (7571439 * 65899 = 498950258661)
-; 12: bani 5 16777215 5 \ r5 = r5 & 16777215 (498950258661 & 16777215 = 12632037)
-; 13: gtir 256 2 4      |
-; 14: addr 4 1 1        |
-; 15: addi 1 1 1        |
-; 16: seti 27 1 1       |
-; 17: seti 0 2 4        \ if 256 > r2, jump to 28, else r4 = 0
-; 18: addi 4 1 3        \ r3 = r4 + 1
-; 19: muli 3 256 3      \ r3 = r3 * 256
-; 20: gtrr 3 2 3        |
-; 21: addr 3 1 1        |
-; 22: addi 1 1 1        |
-; 23: seti 25 6 1       |
-; 24: addi 4 1 4        \ if r3 > r2, jump to 26, else r4 = r4 + 1
-; 25: seti 17 8 1       \ jump to 18
-; 26: setr 4 6 2        \ r2 = r4
-; 27: seti 7 4 1        \ jump to 8
-; 28: eqrr 5 0 4        |
-; 29: addr 4 1 1        |
-; 30: seti 5 5 1        \ if r5 = r0, halt, else jump to 6
+; 00: seti 123 0 5
+; 01: bani 5 456 5
+; 02: eqri 5 72 5
+; 03: addr 5 1 1
+; 04: seti 0 0 1
+; 05: seti 0 9 5
+; 06: bori 5 65536 2
+; 07: seti 7571367 9 5
+; 08: bani 2 255 4
+; 09: addr 5 4 5
+; 10: bani 5 16777215 5
+; 11: muli 5 65899 5
+; 12: bani 5 16777215 5
+; 13: gtir 256 2 4
+; 14: addr 4 1 1
+; 15: addi 1 1 1
+; 16: seti 27 1 1
+; 17: seti 0 2 4
+; 18: addi 4 1 3
+; 19: muli 3 256 3
+; 20: gtrr 3 2 3
+; 21: addr 3 1 1
+; 22: addi 1 1 1
+; 23: seti 25 6 1
+; 24: addi 4 1 4
+; 25: seti 17 8 1
+; 26: setr 4 6 2
+; 27: seti 7 4 1
+; 28: eqrr 5 0 4
+; 29: addr 4 1 1
+; 30: seti 5 5 1
