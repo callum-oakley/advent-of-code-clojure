2a75ed09dc34b9a4e4bd14c855b4f49f253c9443
Callum Oakley <hello@callumoakley.net>
2022-12-07 07:00:51 +0000

    2022 07

---
+++ src/aoc/2022/07.clj
@@ -0,0 +1,47 @@
+(ns aoc.2022.07
+  (:require
+   [aoc.search :as search]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (->> (str/split-lines s)
+       (map #(mapv read-string (re-seq #"\S+" %)))
+       (reduce
+        (fn [[fs path] line]
+          (cond
+            (= '[$ cd ..] line) [fs (pop path)]
+            (= '[$ cd] (take 2 line)) [fs (conj path (line 2))]
+            (= '[$ ls] line) [fs path]
+            (= 'dir (line 0)) [(update-in fs path assoc (line 1) {}) path]
+            :else [(update-in fs path assoc (line 1) (line 0)) path]))
+        [{} []])
+       first))
+
+(defn size [fs]
+  (if (int? fs) fs (apply + (map size (vals fs)))))
+
+(defn part-* [fs]
+  (map size (search/bft fs #(filter map? (vals %)))))
+
+(defn part-1* [fs]
+  (apply + (filter #(<= % 100000) (part-* fs))))
+
+(defn part-2* [fs]
+  (let [free (- 70000000 (size fs))]
+    (apply min (filter #(<= 30000000 (+ free %)) (part-* fs)))))
+
+(defn part-1 []
+  (->> "input/2022/07" slurp parse part-1*))
+
+(defn part-2 []
+  (->> "input/2022/07" slurp parse part-2*))
+
+(def example
+  "$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls
+   dir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..
+   $ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k")
+
+((deftest test-example
+   (is (= 95437 (part-1* (parse example))))
+   (is (= 24933642 (part-2* (parse example))))))
