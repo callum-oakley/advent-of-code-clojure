cdb35a5fa53de5bd291175f5232f9f2d2ff16e8e
Callum Oakley <hello@callumoakley.net>
2022-12-09 07:12:53 +0000

    2022 09

---
+++ src/aoc/2022/09.clj
@@ -0,0 +1,41 @@
+(ns aoc.2022.09
+  (:require
+   [aoc.vector :refer [+v -v chessboard-distance]]
+   [aoc.grid :refer [north east south west]]
+   [clojure.math :as math]
+   [clojure.test :refer [deftest is]]))
+
+(defn parse [s]
+  (->> s (re-seq #"\w+") (map read-string) (partition 2)))
+
+(defn follow [tail head]
+  (if (<= (chessboard-distance tail head) 1)
+    tail
+    (+v tail (mapv #(int (math/signum %)) (-v head tail)))))
+
+(defn move [rope dir]
+  (reduce (fn [rope i] (update rope i follow (rope (dec i))))
+          (update rope 0 +v dir)
+          (range 1 (count rope))))
+
+(defn part-* [length instructions]
+  (count
+   (first
+    (reduce (fn [[visited rope] [dir n]]
+              (if (zero? n)
+                [visited rope]
+                (let [rope (move rope (case dir U north R east D south L west))]
+                  (recur [(conj visited (peek rope)) rope] [dir (dec n)]))))
+            [#{} (vec (repeat length [0 0]))]
+            instructions))))
+
+(defn part-1 []
+  (->> "input/2022/09" slurp parse (part-* 2)))
+
+(defn part-2 []
+  (->> "input/2022/09" slurp parse (part-* 10)))
+
+(deftest test-example
+  (is (= 13 (part-* 2 (parse "R 4 U 4 L 3 D 1 R 4 D 1 L 5 R 2"))))
+  (is (= 1 (part-* 10 (parse "R 4 U 4 L 3 D 1 R 4 D 1 L 5 R 2"))))
+  (is (= 36 (part-* 10 (parse "R 5 U 8 L 8 D 3 R 17 D 10 L 25 U 20")))))
